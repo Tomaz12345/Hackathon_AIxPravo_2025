b0e4e738770546ab392594dd21dc4dabe0a7eb3c
Tomaz12345 <tomazpoljansek12@gmail.com>
2025-03-02 20:41:34 +0100

    added example, fix empty result

--- backend/api/utils/other.py
+++ backend/api/utils/other.py
@@ -4,6 +4,7 @@ def get_message_status(resp_message):
     # Loop through the statuses in reverse order and check if they appear in the resp_message
     for status in reversed(statuses):
         if status.lower() in resp_message.lower():
+            print(f"status: {status}")
             return status
     
     # Default to "caution" if none of the statuses appear
